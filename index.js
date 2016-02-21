d9662977ae6643f7233c1ea9ba00d29d3864d8ac
Curran <curran.kelleher@gmail.com>
2016-02-20 23:24:19 -0800

    Refactor error messages

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 // UMD boilerplate (from Rollup)
 (function (global, factory) {
   typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() :
-  typeof define === "function" && define.amd ? define(factory) :
-  (global.ReactiveProperty = factory());
+  typeof define === "function" && define.amd ? define(factory) : (global.ReactiveProperty = factory());
 }(this, function () { "use strict";
+
+  // Error messages for exceptions thrown.
+  var errors = {
+    tooManyArgsConstructor: "ReactiveProperty(value) accepts only a single argument, the initial value.",
+    tooManyArgsSetter: "reactiveProperty(newValue) accepts only a single argument, the new value.",
+    onNonFunction: "ReactiveProperty.on(listener) only accepts functions, not values.",
+    onArgs: "ReactiveProperty.on(listener) accepts exactly one argument, the listener function."
+  };
 
   // This function generates a getter-setter with change listeners.
   return function ReactiveProperty(value){
-
-    if(arguments.length > 1){
-      throw new Error("ReactiveProperty(value) accepts only a single argument, the initial value.");
-    }
-
-    var listeners;
+    var listeners, i;
+    if(arguments.length > 1) { throw Error(errors.tooManyArgsConstructor); }
 
     function property(newValue){
-      if(arguments.length > 1){
-        throw new Error("reactiveProperty(newValue) accepts only a single argument, the new value.");
-      }
+      if(arguments.length > 1) { throw Error(errors.tooManyArgsSetter); }
       if(arguments.length === 1){
         value = newValue;
         if(listeners){
-          for(var i = 0; i < listeners.length; i++){
-            listeners[i](value);
-          }
+          for(i = 0; i < listeners.length; i++){ listeners[i](value); }
         }
       } else {
         return value;
@@ -31,19 +30,10 @@
     }
 
     property.on = function (listener){
-      if(typeof listener !== "function"){
-        throw new Error("ReactiveProperty.on(listener) only accepts functions, not values.");
-      }
-      if(arguments.length > 1 || arguments.length === 0){
-        throw new Error("ReactiveProperty.on(listener) accepts one argument, the listener function.");
-      }
-      if(!listeners){
-        listeners = [];
-      }
-      listeners.push(listener)
-      if(typeof(value) !== "undefined" && value !== null){
-        listener(value);
-      }
+      if(typeof listener !== "function"){ throw Error(errors.onNonFunction); }
+      if(arguments.length > 1 || arguments.length === 0){ throw Error(errors.onArgs); }
+      (listeners ? listeners : (listeners = [])).push(listener);
+      if(typeof(value) !== "undefined"){ listener(value); }
       return listener;
     };
 
