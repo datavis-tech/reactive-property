b747ac838d77727ebab968e06c6bb442ae3fecf0
Curran <curran.kelleher@gmail.com>
2016-02-20 19:23:25 -0800

    Add UMD

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-// This function generates a getter-setter with change listeners.
-module.exports = function ReactiveProperty(value){
+// UMD boilerplate (from Rollup)
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.ReactiveProperty = factory());
+}(this, function () { 'use strict';
 
-  var listeners;
+  // This function generates a getter-setter with change listeners.
+  function ReactiveProperty(value){
 
-  function property(newValue){
-    if(arguments.length > 0){
-      value = newValue;
-      if(listeners){
-        for(var i = 0; i < listeners.length; i++){
-          listeners[i](value);
+    var listeners;
+
+    function property(newValue){
+      if(arguments.length > 0){
+        value = newValue;
+        if(listeners){
+          for(var i = 0; i < listeners.length; i++){
+            listeners[i](value);
+          }
         }
+      } else {
+        return value;
       }
-    } else {
-      return value;
     }
+
+    property.on = function (listener){
+      if(typeof listener !== "function"){
+        throw new Error("ReactiveProperty.on(listener) only accepts functions, not values.");
+      }
+      if(arguments.length > 1 || arguments.length === 0){
+        throw new Error("ReactiveProperty.on(listener) accepts one argument, the listener function.");
+      }
+      if(!listeners){
+        listeners = [];
+      }
+      listeners.push(listener)
+      if(typeof(value) !== "undefined" && value !== null){
+        listener(value);
+      }
+      return listener;
+    };
+
+    property.off = function (listener){
+      if(listeners){
+        listeners = listeners.filter(function (listener){
+          listener !== listener;
+        });
+      }
+    };
+
+    return property;
   }
 
-  property.on = function (listener){
-    if(typeof listener !== "function"){
-      throw new Error("ReactiveProperty.on(listener) only accepts functions, not values.");
-    }
-    if(arguments.length > 1 || arguments.length === 0){
-      throw new Error("ReactiveProperty.on(listener) accepts one argument, the listener function.");
-    }
-    if(!listeners){
-      listeners = [];
-    }
-    listeners.push(listener)
-    if(typeof(value) !== "undefined" && value !== null){
-      listener(value);
-    }
-    return listener;
-  };
+  return ReactiveProperty;
 
-  property.off = function (listener){
-    if(listeners){
-      listeners = listeners.filter(function (listener){
-        listener !== listener;
-      });
-    }
-  };
-
-  return property;
-}
+}));
