bb409235e4213b08d1c76f97a54b57e6820fe0bc
Curran <curran.kelleher@gmail.com>
2016-02-22 10:01:42 -0800

    Add CDN-ish files for v0.5.0

---
+++ reactive-property-v0.5.0.js
@@ -0,0 +1,66 @@
+// UMD boilerplate (from Rollup)
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() :
+  typeof define === "function" && define.amd ? define(factory) : (global.ReactiveProperty = factory());
+}(this, function () { "use strict";
+
+  // Error messages for exceptions thrown.
+  var errors = {
+    tooManyArgsConstructor: "ReactiveProperty(value, [context]) accepts up to two arguments, the initial value and (optionally) the context object.",
+    tooManyArgsSetter: "reactiveProperty(newValue) accepts only a single argument, the new value.",
+    onNonFunction: "ReactiveProperty.on(listener) only accepts functions, not values.",
+    onArgs: "ReactiveProperty.on(listener) accepts exactly one argument, the listener function."
+  };
+
+  // This function generates a getter-setter with change listeners.
+  return function ReactiveProperty(value, context){
+    var listeners, i;
+    if(arguments.length > 2) { throw Error(errors.tooManyArgsConstructor); }
+
+    function property(newValue){
+      if(arguments.length > 1) { throw Error(errors.tooManyArgsSetter); }
+      if(arguments.length === 1){
+        value = newValue;
+        if(listeners){
+          for(i = 0; i < listeners.length; i++){ listeners[i](value); }
+        }
+        return context;
+      } else {
+        return value;
+      }
+    }
+
+    property.on = function (listener){
+
+      if(typeof listener !== "function"){
+        throw Error(errors.onNonFunction);
+      }
+
+      if(arguments.length > 1 || arguments.length === 0){
+        throw Error(errors.onArgs);
+      }
+
+      if(!listeners){
+        listeners = [];
+      }
+
+      listeners.push(listener);
+
+      if(typeof(value) !== "undefined"){
+        listener.call(context, value);
+      }
+
+      return listener;
+    };
+
+    property.off = function (listenerToRemove){
+      if(listeners){
+        listeners = listeners.filter(function (listener){
+          return listener !== listenerToRemove;
+        });
+      }
+    };
+
+    return property;
+  }
+}));
