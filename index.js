455aaf13a9f15e831f04da5938b2ef7c8014e66e
Curran <curran.kelleher@gmail.com>
2016-02-24 00:27:36 -0500

    Comment code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
   // Error messages for exceptions thrown.
   var errors = {
-    tooManyArgsConstructor: "ReactiveProperty(value) accepts only one argument, the initial value.",
+    tooManyArgsConstructor: "ReactiveProperty(value) accepts only a single argument, the initial value.",
     tooManyArgsSetter: "reactiveProperty(newValue) accepts only a single argument, the new value.",
     onNonFunction: "ReactiveProperty.on(listener) only accepts functions, not values.",
     onArgs: "ReactiveProperty.on(listener) accepts exactly one argument, the listener function."
@@ -14,52 +14,76 @@
 
   // This function generates a getter-setter with change listeners.
   return function ReactiveProperty(value){
+
+    // An array of registered listener functions.
     var listeners;
     
+    // Check for too many arguments.
     if(arguments.length > 2) {
       throw Error(errors.tooManyArgsConstructor);
     }
 
-    function property(newValue){
+    // This is the reactive property function that gets returned.
+    function reactiveProperty(newValue){
+    
+      // Check for too many arguments.
       if(arguments.length > 1) {
         throw Error(errors.tooManyArgsSetter);
-      } else if(arguments.length === 1){
+      }
+      
+      // This implements the setter part of the setter-getter.
+      if(arguments.length === 1){
+
+        // Track the new value internally.
         value = newValue;
+
+        // Notify registered listeners.
         if(listeners){
           for(var i = 0; i < listeners.length; i++){
             listeners[i](value);
           }
         }
+
+        // Support method chaining by returning 'this'.
         return this;
-      } else {
-        return value;
       }
+
+      // This implements the getter part of the setter-getter.
+      return value;
     }
 
-    property.on = function (listener){
+    // Registers a new listener to receive updates.
+    reactiveProperty.on = function (listener){
 
+      // Check for invalid types.
       if(typeof listener !== "function"){
         throw Error(errors.onNonFunction);
       }
 
+      // Check for wrong number of arguments.
       if(arguments.length > 1 || arguments.length === 0){
         throw Error(errors.onArgs);
       }
 
+      // If no listeners have been added yet, initialize the array.
       if(!listeners){
         listeners = [];
       }
 
+      // Register the listener.
       listeners.push(listener);
 
+      // If there is an initial value, invoke the listener immediately.
       if(typeof(value) !== "undefined" && value !== null){
         listener(value);
       }
 
+      // For convenience, the listener is returned.
       return listener;
     };
 
-    property.off = function (listenerToRemove){
+    // Unregisters the given listener function.
+    reactiveProperty.off = function (listenerToRemove){
       if(listeners){
         listeners = listeners.filter(function (listener){
           return listener !== listenerToRemove;
@@ -67,6 +91,6 @@
       }
     };
 
-    return property;
+    return reactiveProperty;
   }
 }));
